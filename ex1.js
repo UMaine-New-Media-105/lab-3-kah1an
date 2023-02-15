aca7cf1a7575e1bb9074d68392e94331dc32484c
kah1an <124199658+kah1an@users.noreply.github.com>
2023-02-15 14:16:08 -0500

    created ex1

---
+++ ex1.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  noStroke();
+  
+  
+
+  rect(170, 150, 20, 100);
+  ellipse(155, 245, 70, 50);
+  rect(170, 150, 100, 20);
+  rect(260, 150, 20, 100);
+  ellipse(245, 245, 70, 50);
+  
+  if (mouseX < 133.33) {
+    fill('orchid')
+  }else if (mouseX >= 133.33 && mouseX < 266.66) {
+    fill('mediumorchid')
+  }else {
+    fill ('darkorchid')
+  }
+  
+  
+  
+}
